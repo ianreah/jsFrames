eba1871899134fd1e8ab6d5ef9321fd36a4680ea
Ian Reah <ian.reah@gmail.com>
2013-01-07 12:40:11 +0000

    first

---
+++ example/main.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+    function triangleWave(time, period, amplitude) {
+        return Math.abs(2 * (time / period - Math.floor(time / period + 0.5))) * amplitude;
+    }
+
+    var ballRadius = 10;
+    var speed = 10000; // milliseconds to cover canvas height (and back again!)
+
+    var theCanvas = $('#theCanvas')[0];
+    var drawingContext = theCanvas.getContext("2d");
+
+    resize();
+    $(window).resizeAsObservable()
+			 .throttle(250)
+			 .subscribe(resize);
+
+    function resize() {
+        theCanvas.width = $(window).width();
+        theCanvas.height = $(window).height();
+    }
+
+    jsFrames.registerAnimation(function (thisFrameTimestamp) {
+        var position = triangleWave(thisFrameTimestamp - startTime, speed, theCanvas.height - ballRadius * 2) + ballRadius;
+
+        drawingContext.clearRect(0, 0, theCanvas.width, theCanvas.height);
+
+	    drawingContext.beginPath();
+	    drawingContext.arc(theCanvas.width / 2, position, ballRadius, 0, Math.PI * 2, false);
+	    drawingContext.closePath();
+
+	    drawingContext.fillStyle = "#FF0000";
+	    drawingContext.fill();
+	});
+
+	var theFpsDisplay = $('#fps');
+	jsFrames.fpsStream.subscribe(function (fps) {
+		theFpsDisplay.html("FPS: " + fps);
+	});
+	
+	var startTime = jsFrames.start();
+});
---
+++ jsFrames.js
@@ -0,0 +1,66 @@
+﻿// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+(function () {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+                                   || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function (callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+}());
+
+// jsFrames main module
+window.jsFrames = (function (rx, jsF) {
+	var animations = [];
+	
+	var filterStrength = 20;
+	var frameTime = 0, lastFrameTimestamp = 0;
+
+	function animate(timeStamp) {
+		requestAnimationFrame(animate);
+		
+		for (var i = 0; i < animations.length; i++) {
+			animations[i](timeStamp);
+		}
+		
+	    // http://stackoverflow.com/questions/4787431/check-fps-in-js/5111475#5111475
+		var timeSinceLastFrame = timeStamp - lastFrameTimestamp;
+		frameTime += (timeSinceLastFrame - frameTime) / filterStrength;
+		lastFrameTimestamp = timeStamp;
+	}
+
+	jsF.registerAnimation = function(animation) {
+		animations.push(animation);
+	};
+
+	jsF.start = function () {
+		lastFrameTimestamp = Date.now();
+		
+		animate(lastFrameTimestamp);
+		return lastFrameTimestamp;
+	};
+	
+	jsF.fpsStream = rx.Observable.interval(1000).select(function () {
+	    return (1000 / frameTime).toFixed();
+	}).distinctUntilChanged();
+
+	return jsF;
+}(window.Rx, window.jsFrames || {}));
