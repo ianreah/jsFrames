3e782dd0cae2b5543ae936e5e9048a9ecb894cd9
Ian Reah <ian.reah@gmail.com>
2013-02-07 08:52:46 +0000

    Use un-minified versions of library files for development
    ...I'll minimize them anyway

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --warning_level QUIET --js third-party\rx.min.js --js third-party\rx.time.min.js --js jsFrames.js --js_output_file jsFrames.min.js --externs externs.js
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --warning_level QUIET --js third-party\rx.js --js third-party\rx.time.js --js jsFrames.js --js_output_file jsFrames.min.js --externs externs.js
