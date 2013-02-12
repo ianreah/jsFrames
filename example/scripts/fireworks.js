2645702e644fef56aa2e6e200564acd9e63228c9
Ian Reah <ian.reah@gmail.com>
2013-02-12 13:16:02 +0000

    Fireworks example demonstrating Particle

---
+++ example/scripts/fireworks.js
@@ -0,0 +1,66 @@
+﻿$(function () {
+    var particles = [];
+
+	var createParticle = function(position, velocity, color, exploded) {
+		position = position || {};
+		velocity = velocity || {};
+		
+		var particle = new jsFrames.Particle(position, velocity, color);
+		particle.exploded = exploded;
+		
+		particles.push(particle);
+	};
+
+    var theCanvas = $('#theCanvas')[0];
+    var drawingContext = theCanvas.getContext("2d");
+
+    // TODO: touch event also
+    $('#theCanvas').mouseup(function () {
+    	createParticle(
+    		{ x: theCanvas.width * 0.5, y: theCanvas.height + 10 }, // firework start position
+            { x: Math.random() * 3 - 1.5, y: Math.random() * 5 - 10 }, // velocity
+            "hsl(" + Math.random()*255 + ",100%,60%)"); // color
+    });
+
+    jsFrames.registerAnimation(function () {
+        drawingContext.fillStyle = "rgba(0,0,0,0.2)";
+        drawingContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
+
+        var index = particles.length;
+        while (index--) {
+            var particle = particles[index];
+
+            if (particle.update()) {
+                particles.splice(index, 1);
+
+                if (!particle.exploded) {
+                    var count = 100;
+                    var angle = (Math.PI * 2) / count;
+                    while (count--) {
+
+                        var randomVelocity = 4 + Math.random() * 4;
+                        var particleAngle = count * angle;
+
+                        createParticle(
+                          particle.position,
+                          {
+                              x: Math.cos(particleAngle) * randomVelocity,
+                              y: Math.sin(particleAngle) * randomVelocity
+                          },
+                          particle.color,
+                          true);
+                    }
+                }
+            }
+
+            particle.render(drawingContext);
+        }
+    });
+
+    var theFpsDisplay = $('#fps');
+    jsFrames.onFpsUpdate(function (fps) {
+        theFpsDisplay.html("FPS: " + fps);
+    });
+
+    jsFrames.start();
+});
